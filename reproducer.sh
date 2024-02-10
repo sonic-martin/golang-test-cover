1f132cb5ef1cd338cb09c4640d494ff2ec76f8c1
Martin Schallnahs <14333891+sonic-martin@users.noreply.github.com>
2024-02-10 23:14:11 +0100

    exit status

--- reproducer.sh
+++ reproducer.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 go test -v ./... -coverprofile=coverage.out -coverpkg=./internal/... -covermode count
+echo $?
 go tool cover -html=coverage.out -o coverage.html
 go tool cover -func=coverage.out
