438cafdfa7e194c0441f3cc5522169a5a9e21e90
Martin Schallnahs <14333891+sonic-martin@users.noreply.github.com>
2024-02-10 23:23:12 +0100

    workarounds

--- reproducer.sh
+++ reproducer.sh
@@ -4,3 +4,11 @@ go test -v ./... -coverprofile=coverage.out -coverpkg=./internal/... -covermode 
 echo $?
 go tool cover -html=coverage.out -o coverage.html
 go tool cover -func=coverage.out
+
+# workaround 1: changes semantic of the coverage result
+go test -v ./... -coverprofile=coverage.out -coverpkg=./... -covermode count
+echo $?
+
+# workaround 2: doesnt uses the changes in go 1.22.0
+GOEXPERIMENT=nocoverageredesign go test -v ./... -coverprofile=coverage.out -coverpkg=./internal/... -covermode count
+echo $?
